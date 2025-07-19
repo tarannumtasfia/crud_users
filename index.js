87be2c4f6995d77e64e6a83ecfd4482c0846ab73
tarannumtasfia <tasfiatarannum@yahoo.com>
2025-07-19 14:21:04 +0600

    commited

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ import authUserRoutes from './routes/authUserRoute.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://yourfrontenddomain.vercel.app'],
+  credentials: true,
+}));
 app.use(express.json());  // <-- use built-in JSON parser
 
 const PORT = process.env.PORT || 5000;
